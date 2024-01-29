89831fe5e3eb891a8431c9ff31ab82177d5af2e1
Sérgio Santos <me@s3rgiosan.com>
2024-01-29 23:41:04 +0000

    Fix format

--- src/view.js
+++ src/view.js
@@ -1,6 +1,6 @@
-import littlefoot from 'littlefoot'
+import littlefoot from 'littlefoot';
 
 littlefoot({
 	scope: '[data-fn]',
-	anchorPattern: /[0-9a-fA-F-]+/gi
+	anchorPattern: /[0-9a-fA-F-]+/gi,
 });
