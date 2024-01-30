eb38a9921847e9b9f7ec33dcf3cd21458db5dcfb
Sérgio Santos <me@s3rgiosan.com>
2024-01-30 00:02:49 +0000

    Parse block options and pass them to littlefoot

--- src/view.js
+++ src/view.js
@@ -1,6 +1,25 @@
 import littlefoot from 'littlefoot';
 
-littlefoot({
-	scope: '[data-fn]',
-	anchorPattern: /[0-9a-fA-F-]+/gi,
+document.addEventListener('DOMContentLoaded', () => {
+	const footnotes = document.querySelector('.wp-block-footnotes[data-littlefoot]');
+	if (footnotes) {
+		let options = {};
+		try {
+			options = JSON.parse(footnotes.dataset.littlefoot);
+		} catch (e) {
+			// eslint-disable-next-line no-console
+			console.error(e);
+			return;
+		}
+
+		if (options.isLittlefootEnabled) {
+			littlefoot({
+				...{
+					scope: '[data-fn]',
+					anchorPattern: /[0-9a-fA-F-]+/gi,
+				},
+				...options,
+			});
+		}
+	}
 });
