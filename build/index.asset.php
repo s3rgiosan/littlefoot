5adb80786119a83bcad2e9024fc3b3aaa4d8ebf3
Sérgio Santos <me@s3rgiosan.com>
2024-01-30 00:03:07 +0000

    Add compiled assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'd8f58e448e93504dfa21');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-hooks', 'wp-i18n'), 'version' => 'b775344edc0bd7e3ed3a');
